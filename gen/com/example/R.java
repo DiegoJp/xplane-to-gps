f8f535790707a6d9a12834a1d83f8b6b9277c85a
Kevin A. Roll <kroll@appropel.com>
2012-05-01 21:19:19 -0400

    Added section headers.

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -22,17 +22,19 @@ public final class R {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int altitude=0x7f030009;
+        public static final int altitude=0x7f03000b;
         public static final int app_name=0x7f030000;
-        public static final int data_instructions=0x7f030002;
-        public static final int groundspeed=0x7f03000b;
-        public static final int heading=0x7f03000a;
-        public static final int headings=0x7f030004;
-        public static final int ip_instructions=0x7f030001;
-        public static final int latitude=0x7f030007;
-        public static final int latlon=0x7f030005;
-        public static final int longitude=0x7f030008;
-        public static final int mock_location=0x7f030006;
-        public static final int speeds=0x7f030003;
+        public static final int data=0x7f030008;
+        public static final int data_instructions=0x7f030003;
+        public static final int groundspeed=0x7f03000d;
+        public static final int heading=0x7f03000c;
+        public static final int headings=0x7f030005;
+        public static final int instructions=0x7f030001;
+        public static final int ip_instructions=0x7f030002;
+        public static final int latitude=0x7f030009;
+        public static final int latlon=0x7f030006;
+        public static final int longitude=0x7f03000a;
+        public static final int mock_location=0x7f030007;
+        public static final int speeds=0x7f030004;
     }
 }
