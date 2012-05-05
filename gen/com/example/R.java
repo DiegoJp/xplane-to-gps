82333d45079d51c80654c403619a56e2f8e8ca33
Kevin A. Roll <kroll@appropel.com>
2012-05-04 22:17:07 -0400

    Added icons to tabs.

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -11,35 +11,43 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int font_size=0x7f030000;
+        public static final int font_size=0x7f040000;
+    }
+    public static final class drawable {
+        public static final int ic_tab_gear=0x7f020000;
+        public static final int ic_tab_gear_active=0x7f020001;
+        public static final int ic_tab_gear_inactive=0x7f020002;
+        public static final int ic_tab_plane=0x7f020003;
+        public static final int ic_tab_plane_active=0x7f020004;
+        public static final int ic_tab_plane_inactive=0x7f020005;
     }
     public static final class id {
-        public static final int altitude_view=0x7f050002;
-        public static final int groundspeed_view=0x7f050004;
-        public static final int heading_view=0x7f050003;
-        public static final int ip_text_view=0x7f050005;
-        public static final int latitude_view=0x7f050000;
-        public static final int longitude_view=0x7f050001;
+        public static final int altitude_view=0x7f060002;
+        public static final int groundspeed_view=0x7f060004;
+        public static final int heading_view=0x7f060003;
+        public static final int ip_text_view=0x7f060005;
+        public static final int latitude_view=0x7f060000;
+        public static final int longitude_view=0x7f060001;
     }
     public static final class layout {
-        public static final int data=0x7f020000;
-        public static final int instructions=0x7f020001;
-        public static final int main=0x7f020002;
+        public static final int data=0x7f030000;
+        public static final int instructions=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
-        public static final int altitude=0x7f04000b;
-        public static final int app_name=0x7f040000;
-        public static final int data=0x7f040008;
-        public static final int data_instructions=0x7f040003;
-        public static final int groundspeed=0x7f04000d;
-        public static final int heading=0x7f04000c;
-        public static final int headings=0x7f040005;
-        public static final int instructions=0x7f040001;
-        public static final int ip_instructions=0x7f040002;
-        public static final int latitude=0x7f040009;
-        public static final int latlon=0x7f040006;
-        public static final int longitude=0x7f04000a;
-        public static final int mock_location=0x7f040007;
-        public static final int speeds=0x7f040004;
+        public static final int altitude=0x7f05000b;
+        public static final int app_name=0x7f050000;
+        public static final int data=0x7f050008;
+        public static final int data_instructions=0x7f050003;
+        public static final int groundspeed=0x7f05000d;
+        public static final int heading=0x7f05000c;
+        public static final int headings=0x7f050005;
+        public static final int instructions=0x7f050001;
+        public static final int ip_instructions=0x7f050002;
+        public static final int latitude=0x7f050009;
+        public static final int latlon=0x7f050006;
+        public static final int longitude=0x7f05000a;
+        public static final int mock_location=0x7f050007;
+        public static final int speeds=0x7f050004;
     }
 }
--- src/com/appropel/xplanegps/activity/MainActivity.java
+++ src/com/appropel/xplanegps/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.appropel.xplanegps.activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.widget.TabHost;
 import com.example.R;
@@ -18,16 +19,19 @@ public final class MainActivity extends RoboTabActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        Resources res = getResources();
         TabHost tabHost = getTabHost();
         TabHost.TabSpec spec;
 
         // Data tab
-        spec = tabHost.newTabSpec("data").setIndicator(getString(R.string.data))
+        spec = tabHost.newTabSpec("data")
+                .setIndicator(getString(R.string.data), res.getDrawable(R.drawable.ic_tab_plane))
                 .setContent(new Intent(this, DataActivity.class));
         tabHost.addTab(spec);
 
         // Instructions tab
-        spec = tabHost.newTabSpec("instructions").setIndicator(getString(R.string.instructions))
+        spec = tabHost.newTabSpec("instructions")
+                .setIndicator(getString(R.string.instructions), res.getDrawable(R.drawable.ic_tab_gear))
                 .setContent(new Intent(this, InstructionActivity.class));
         tabHost.addTab(spec);
     }
