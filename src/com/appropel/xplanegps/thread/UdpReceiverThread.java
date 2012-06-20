a4f382b60713a51d368c83a994231d313a1b9a31
Kevin A. Roll <kroll@appropel.com>
2012-06-20 00:06:26 -0400

    Made changes to support X-Plane 9.

--- src/com/appropel/xplanegps/thread/UdpReceiverThread.java
+++ src/com/appropel/xplanegps/thread/UdpReceiverThread.java
@@ -19,14 +19,17 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public final class UdpReceiverThread implements Runnable
 {
-    /** Log tag. */
-    private static final String TAG = UdpReceiverThread.class.getName();
-
     /** Conversion factor from knots to m/s. */
     public static final float KNOTS_TO_M_S = 0.514444444f;
 
     /** Conversion factor from feet to meters. */
     public static final float FEET_TO_METERS = 0.3048f;
+
+    /** Packet header. */
+    public static final String PACKET_HEADER = "DATA";
+
+    /** Log tag. */
+    private static final String TAG = UdpReceiverThread.class.getName();
 
     /** Data buffer for packet reception. */
     private byte[] data = new byte[1024];
@@ -67,60 +70,68 @@ public final class UdpReceiverThread implements Runnable
             {
                 try
                 {
-                    socket.receive(packet);
-                }
-                catch (final InterruptedIOException iioex)
-                {
-                    continue;   // No packet was received so continue loop
-                }
-
-                // Verify that this is a valid packet from X-Plane by examining the first 5 bytes.
-                if (data[0] != 0x44 || data[1] != 0x41 || data[2] != 0x54 || data[3] != 0x41 || data[4] != 0x40)
-                {
-                    continue;
-                }
-
-                // Extract data packets from buffer.
-                ByteBuffer buffer = ByteBuffer.wrap(data);
-                buffer.order(ByteOrder.LITTLE_ENDIAN);
-
-                int index = 5;
-                List<DataPacket> dataPackets = new ArrayList<DataPacket>();
-                while (index + DataPacket.LENGTH <= packet.getLength())
-                {
-                    DataPacket dataPacket = new DataPacket(buffer, index);
-                    dataPackets.add(dataPacket);
-                    index += DataPacket.LENGTH;
-                }
-
-                // Transfer data values into a Location object.
-                Location location = new Location(mockLocationProvider);
-                for (DataPacket dataPacket : dataPackets)
-                {
-                    switch (dataPacket.getIndex())
+                    try
                     {
-                        case 3:     // speeds
-                            location.setSpeed(dataPacket.getValues()[3] * KNOTS_TO_M_S);
-                            break;
-                        case 17:    // pitch, roll, headings
-                            location.setBearing(dataPacket.getValues()[2]);
-                            break;
-                        case 20:    // lat, lon, altitude
-                            location.setLatitude(dataPacket.getValues()[0]);
-                            location.setLongitude(dataPacket.getValues()[1]);
-                            location.setAltitude(dataPacket.getValues()[2] * FEET_TO_METERS);
-                            break;
-                        default:
-                            break;
+                        socket.receive(packet);
                     }
-                }
+                    catch (final InterruptedIOException iioex)
+                    {
+                        continue;   // No packet was received so continue loop
+                    }
 
-                // set the time in the location. If the time on this location
-                // matches the time on the one in the previous set call, it will be
-                // ignored
-                location.setTime(System.currentTimeMillis());
-                locationManager.setTestProviderLocation(mockLocationProvider, location);
-                mainApplication.setLocation(location);
+                    // Extract data packets from buffer.
+                    ByteBuffer buffer = ByteBuffer.wrap(data);
+                    buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+                    // Verify that this is a valid packet from X-Plane by examining the first 4 bytes.
+                    if (!PACKET_HEADER.equals(new String(buffer.array(), 0, 4)))
+                    {
+                        continue;
+                    }
+
+                    int index = 5;
+                    List<DataPacket> dataPackets = new ArrayList<DataPacket>();
+                    while (index + DataPacket.LENGTH <= packet.getLength())
+                    {
+                        DataPacket dataPacket = new DataPacket(buffer, index);
+                        dataPackets.add(dataPacket);
+                        index += DataPacket.LENGTH;
+                    }
+
+                    // Transfer data values into a Location object.
+                    Location location = new Location(mockLocationProvider);
+                    for (DataPacket dataPacket : dataPackets)
+                    {
+                        switch (dataPacket.getIndex())
+                        {
+                            case 3:     // speeds
+                                location.setSpeed(dataPacket.getValues()[3] * KNOTS_TO_M_S);
+                                break;
+                            case 17:    // pitch, roll, headings (X-Plane 10)
+                            case 18:    // pitch, roll, headings (X-Plane 9)
+                                location.setBearing(dataPacket.getValues()[2]);
+                                break;
+                            case 20:    // lat, lon, altitude
+                                location.setLatitude(dataPacket.getValues()[0]);
+                                location.setLongitude(dataPacket.getValues()[1]);
+                                location.setAltitude(dataPacket.getValues()[2] * FEET_TO_METERS);
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    // set the time in the location. If the time on this location
+                    // matches the time on the one in the previous set call, it will be
+                    // ignored
+                    location.setTime(System.currentTimeMillis());
+                    locationManager.setTestProviderLocation(mockLocationProvider, location);
+                    mainApplication.setLocation(location);
+                }
+                catch (Exception e)
+                {
+                    Log.w("Exception in receiver loop, continuing", e);
+                }
             }
         }
         catch (Exception ex)
