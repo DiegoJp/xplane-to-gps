c99e8db0b3a98131c506bcf2b46c131784933501
Kevin A. Roll <kroll@appropel.com>
2012-07-09 21:27:31 -0400

    Added notification in status bar when service is active.

--- src/com/appropel/xplanegps/service/DataService.java
+++ src/com/appropel/xplanegps/service/DataService.java
@@ -1,7 +1,13 @@
 package com.appropel.xplanegps.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import com.appropel.xplanegps.R;
+import com.appropel.xplanegps.activity.MainActivity;
 import com.appropel.xplanegps.guice.MainApplication;
 import com.appropel.xplanegps.thread.UdpReceiverThread;
 import com.google.inject.Inject;
@@ -12,6 +18,9 @@ import roboguice.service.RoboService;
  */
 public final class DataService extends RoboService
 {
+    /** Notification identifier. */
+    private static final int NOTIFICATION_ID = 1;
+
     /** Main application. */
     @Inject
     private MainApplication mainApplication;
@@ -30,6 +39,18 @@ public final class DataService extends RoboService
     {
         udpReceiverThread = new UdpReceiverThread(mainApplication);
         new Thread(udpReceiverThread).start();
+
+        // Create notification.
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification = new Notification(
+                R.drawable.ic_menu_plane, getText(R.string.notification), System.currentTimeMillis());
+        Context context = getApplicationContext();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.setLatestEventInfo(
+                context, getText(R.string.app_name), getText(R.string.notification), contentIntent);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+
         return START_STICKY;
     }
 
@@ -39,5 +60,8 @@ public final class DataService extends RoboService
         super.onDestroy();
         udpReceiverThread.stop();
         udpReceiverThread = null;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
     }
 }
