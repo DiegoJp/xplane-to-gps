a1b691ff54d6599378d0457bab3a928e719d7691
Kevin A. Roll <kroll@appropel.com>
2016-12-07 22:04:19 -0500

    First steps in rearranging for Gradle build.

--- app/src/main/java/com/appropel/xplanegps/view/activity/MainActivity.java
+++ app/src/main/java/com/appropel/xplanegps/view/activity/MainActivity.java
@@ -1,29 +1,18 @@
-package com.appropel.xplanegps.activity;
+package com.appropel.xplanegps.view.activity;
 
+import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TabHost;
+
 import com.appropel.xplanegps.R;
-import com.appropel.xplanegps.guice.MainApplication;
-import com.appropel.xplanegps.utility.SettingsUtility;
-import com.google.inject.Inject;
-import roboguice.activity.RoboTabActivity;
 
 /**
  * Main activity of the application.
  */
-public final class MainActivity extends RoboTabActivity implements TabConstants
+public final class MainActivity extends Activity
 {
     /** Log tag. */
     private static final String TAG = MainActivity.class.getName();
-
-    /** Main application. */
-    @Inject
-    private MainApplication mainApplication;
 
     /** Alert dialog to warn about mock locations. */
     private AlertDialog alertDialog;
@@ -35,30 +24,30 @@ public final class MainActivity extends RoboTabActivity implements TabConstants
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Set up tabs.
-        Resources res = getResources();
-        TabHost tabHost = getTabHost();
-        TabHost.TabSpec spec;
-
-        // Settings tab
-        spec = tabHost.newTabSpec(SETTINGS_TAB_TAG)
-                .setIndicator(getString(R.string.settings), res.getDrawable(R.drawable.ic_tab_tick))
-                .setContent(new Intent(this, SettingsActivity.class));
-        tabHost.addTab(spec);
-
-        // Data tab
-        spec = tabHost.newTabSpec(DATA_TAB_TAG)
-                .setIndicator(getString(R.string.data), res.getDrawable(R.drawable.ic_tab_plane))
-                .setContent(new Intent(this, DataActivity.class));
-        tabHost.addTab(spec);
-
-        // Instructions tab
-        spec = tabHost.newTabSpec(INSTRUCTIONS_TAB_TAG)
-                .setIndicator(getString(R.string.instructions), res.getDrawable(R.drawable.ic_tab_gear))
-                .setContent(new Intent(this, InstructionActivity.class));
-        tabHost.addTab(spec);
+//        // Set up tabs.
+//        Resources res = getResources();
+//        TabHost tabHost = getTabHost();
+//        TabHost.TabSpec spec;
+//
+//        // Settings tab
+//        spec = tabHost.newTabSpec(SETTINGS_TAB_TAG)
+//                .setIndicator(getString(R.string.settings), res.getDrawable(R.drawable.ic_tab_tick))
+//                .setContent(new Intent(this, SettingsActivity.class));
+//        tabHost.addTab(spec);
+//
+//        // Data tab
+//        spec = tabHost.newTabSpec(DATA_TAB_TAG)
+//                .setIndicator(getString(R.string.data), res.getDrawable(R.drawable.ic_tab_plane))
+//                .setContent(new Intent(this, DataActivity.class));
+//        tabHost.addTab(spec);
+//
+//        // Instructions tab
+//        spec = tabHost.newTabSpec(INSTRUCTIONS_TAB_TAG)
+//                .setIndicator(getString(R.string.instructions), res.getDrawable(R.drawable.ic_tab_gear))
+//                .setContent(new Intent(this, InstructionActivity.class));
+//        tabHost.addTab(spec);
     }
-
+/*
     @Override
     protected void onStart()
     {
@@ -98,5 +87,5 @@ public final class MainActivity extends RoboTabActivity implements TabConstants
             alertDialog = null;
         }
     }
-
+*/
 }
